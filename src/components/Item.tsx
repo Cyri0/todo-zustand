2333a88277fccd6961ce4eb07d6f107637acbe30
NT - B-1/6 <you@example.com>
2026-09-22 14:13:17 +0200

    feat: enhance Item component with toggle functionality and update store structure

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -4,10 +4,11 @@ import type { ItemType } from "../types/Item"
 const Item = ({item}:{item: ItemType}) => {
 
   const removeItem = useItemStore((state) => state.removeItem)
+  const toggleItem = useItemStore((state) => state.toggleItem)
 
   return (
-    <div>
-      <p>{item.name}</p>
+    <div className="item">
+      <p onClick={()=>toggleItem(item.id)} className={item.isDone ? "done" : ""}>{item.name}</p>
       <button onClick={()=>removeItem(item.id)}>❌</button>
     </div>
   )
--- src/store/useItemStore.ts
+++ src/store/useItemStore.ts
@@ -4,11 +4,13 @@ import type { ItemType } from "../types/Item";
 type ItemStoreType = {
     items: ItemType[],
     addItem: (item:ItemType)=>void,
-    removeItem: (id: string)=>void
+    removeItem: (id: string)=>void,
+    toggleItem: (id: string)=>void
 }
 
 export const useItemStore = create<ItemStoreType>((set)=>({
     items: [],
     addItem: (item) => set((state) => ({items: [item, ...state.items]})),
-    removeItem: (id) => set((state) => ({items: state.items.filter(item => item.id !== id)}))
+    removeItem: (id) => set((state) => ({items: state.items.filter(item => item.id !== id)})),
+    toggleItem: (id) => set((state) => ({items: state.items.map(item => (item.id === id) ? {...item, isDone: !item.isDone} : item)})),
 }))
